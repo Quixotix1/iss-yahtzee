20679e96f996079e859be9cdcb7e0361227c8e1d
pres1680 <146100034+pres1680@users.noreply.github.com>
2023-10-02 09:32:35 -0400

    Update finishedGame.java

--- finishedGame.java
+++ finishedGame.java
@@ -1,68 +1,157 @@
-package com.mycompany.yahtzee;
-
-import java.util.*;
+package com.mycompany.yahtzee.kateYahtzee;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.ArrayList;
 /**
  *
- * @author terr3890
+ * @author joshg
  */
-public class Checker extends Scoring{
-
-    public static void main(String[] args)
-    {
-        
+public class Game extends Checker {
+    
+    // def classes
+    private  final Random rand = new Random();
+    private  final Scanner in = new Scanner(System.in);
+    
+    // def variables
+    private  int remainingRolls;
+    private  ArrayList<Integer> die;
+    private  final ArrayList<Integer> heldNums;
+    
+    
+    public Game() {
+        this.heldNums = new ArrayList<>();
     }
- 
-    static boolean[] checkPossibilities(ArrayList<Integer> dice, boolean[] taken, boolean noYahtzee) {
-        ArrayList<Integer> numVals = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0));
-        
-        for (int roll : dice) {
-            numVals.set(roll - 1, numVals.get(roll - 1)+ 1);
-        }
-        boolean[] possible = {false, false, false, false, false, false, false, false, false, false, false, false, true}; //possible scores
-       //, 123456, 3oak, 4oak, fhouse, sstraight, lstraight, yahtzee, chance(id default true because it will be taken away if it's been chosen but is never checked for)
-        if (numVals.contains(5)) { //Yahtzee
-            possible[11] = true;
-            possible[6] = true;
-            possible[7] = true;
-        } else if (numVals.contains(4)) { //Four of a kind
-            possible[6] = true;
-            possible[7] = true;
-        } else if (numVals.contains(3)) {
-            possible[6] = true;
-            if (numVals.contains(2)) {//full house
-                possible[8] = true;
+    
+    public ArrayList<Integer> run() {
+        ArrayList<Integer> allDice = new ArrayList<Integer>();
+        remainingRolls = 3;
+        allDice.clear();
+        heldNums.clear();
+        OUTER:
+        while(true){
+            allDice.clear();
+            // random number array, output numbers
+            die = new ArrayList<>();
+            die.addAll(rand.ints(5 - heldNums.size(), 1, 7).boxed().toList());
+            remainingRolls--;
+            System.out.print("Rolls, ");
+            //for(int dice : die) {
+            //   System.out.printf("%d ", dice);
+            //}
+            
+            for (int i = 0; i < 5; i++){
+               allDice.add(0); 
             }
-        }
-        for (int i = 1; i <= dice.size() + 1; i++){//123456 check
-           if (dice.contains(i)){
-             possible[i-1] = true;
-           }
-         
-        }
-     
-       
-        if (numVals.get(2) >= 1 && numVals.get(3) >= 1){
-            if ((numVals.get(0) >= 1 && (numVals.get(1) >= 1)) || (numVals.get(1) >= 1 && numVals.get(4) >= 1) || (numVals.get(4) >= 1 && numVals.get(5) >= 1)) { //small straight
-                possible[9] = true; 
+            for (int i = 0; i < die.size(); i++){
+                allDice.set(i, die.get(i));
             }
-            if ((numVals.get(1) == 1 && numVals.get(4) == 1) && (numVals.get(0) == 1 || numVals.get(5) == 1) ){// large straight
-                possible[10] = true;
+            for (int i = (die.size()); i < (heldNums.size() + die.size()); i++){
+                allDice.set(i,heldNums.get(i - die.size()));
             }
-        }
-        
-        for (int i = 0; i < taken.length; i++){
-            if (i != 11){
-                if (taken[i] == true){
-                    possible[i] = false;
+            for (int i = 0; i < allDice.size(); i++){
+                System.out.print(allDice.get(i) + " ");
+            }
+            // get input
+            INNER:
+            while(true){
+                if(remainingRolls <= 0) break OUTER;
+                System.out.println("""
+                                   
+                                   Select an operation:
+                                   1) reroll
+                                   2) end turn
+                                   3) modify held die""");
+                String input = in.nextLine();
+                
+                switch(input) {
+                    case "1" -> {
+                        break INNER;
+                    }
+                    case "2" -> {
+                        break OUTER;
+                    }
+                    case "3" -> {
+                        modifyHeldDie(die, heldNums);
+                        break;
+                    }
+                    default -> {
+                        System.out.println("Please enter a valid input.");
+                    }
                 }
             }
-            else if (noYahtzee)
-            {
-                possible[i] = false;
-            }
+        }
+        return allDice;
+    }
+    
+    public void modifyHeldDie(ArrayList<Integer> die, ArrayList<Integer> heldDie) {
+        // show die and held die to the player
+        System.out.print("Die: ");
+        for(int dice : die) {
+            System.out.print(dice + " ");
+        }
+        System.out.print("\nHeld Die: ");
+        for(int dice : heldDie) {
+            System.out.print(dice + " ");
         }
         
-        return possible; //return array list with possible combinations
+        System.out.println("""
+                           
+                           What would you like to modify?:
+                           1) hold a dice
+                           2) let go of a dice
+                           3) back""");
+        String input = in.nextLine();
+        switch(input) {
+            case "1" -> {
+                if(die.isEmpty()) {
+                    System.out.println("Error: No dice on table");
+                    break;
+                }
+                while(true){
+                    System.out.println("Which dice would you like to hold?");
+                    for(int i = 0; i < die.size(); i++) {
+                        System.out.printf("%d) %d%n", i + 1, die.get(i));
+                    }
+                    // get die, add to held, remove from die, if error then return
+                    //hold multiple dice at a time
+                    try {
+                        int hold = Integer.parseInt(in.nextLine());
+                        heldDie.add(die.get(hold - 1));
+                        die.remove(hold - 1);
+                    } catch(NumberFormatException e) {
+                        System.out.println("Input not received successfully.");
+                    }
+                    System.out.println("Would you like to hold any more? (y or n)");
+                    if (in.nextLine().equals("n")){
+                        break;
+                    }
+                }
+                break;
+            }
+            case "2" -> {
+                if(heldDie.isEmpty()) {
+                    System.out.println("Error: No dice being held");
+                    break;
+                }
+ *
+                System.out.println("Which dice would you like to let go of?");
+                for(int i = 0; i < heldDie.size(); i++) {
+                    System.out.printf("%d) %d%n", i + 1, heldDie.get(i));
+                }
+
+                // get die, add to held, remove from die, if error then return
+                try {
+                    int remove = Integer.parseInt(in.nextLine());
+                    die.add(heldDie.get(remove - 1));
+                    heldDie.remove(remove - 1);
+                } catch(NumberFormatException e) {
+                    System.out.println("Input not received successfully.");
+                }
+                break;
+            }
+            default -> {
+                return;
+            }
+        }
     }
 }
